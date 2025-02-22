7185ec253fedcd42bfaad182e6e0ac8d275268ee
li zhang <lzhang421@gmail.com>
2025-02-22 16:37:53 -0500

    added abstract rest response.

---
+++ common/src/main/java/com/zhang/common/base/rest/AbstractRestResponse.java
@@ -0,0 +1,47 @@
+package com.zhang.common.base.rest;
+
+public abstract class AbstractRestResponse<T,S> {
+    private String code;
+    private String message;
+    private T data;
+    private S metaData;
+
+    public AbstractRestResponse(String code, String message, T data, S metaData) {
+        this.code = code;
+        this.message = message;
+        this.data = data;
+        this.metaData = metaData;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public void setCode(String code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public S getMetaData() {
+        return metaData;
+    }
+
+    public void setMetaData(S metaData) {
+        this.metaData = metaData;
+    }
+}
