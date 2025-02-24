02d0b94ac7c87447b23d046be20644060d7db30a
li zhang <lzhang421@gmail.com>
2025-02-24 12:52:34 -0500

    created abstract method for collection of dtos

--- common/src/main/java/com/zhang/common/base/rest/response/AbstractAPIResponse.java
+++ common/src/main/java/com/zhang/common/base/rest/response/AbstractAPIResponse.java
@@ -1,6 +1,8 @@
 package com.zhang.common.base.rest.response;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Collection;
+
 
 public abstract class AbstractAPIResponse<T> {
     public ResponseEntity<AbstractRestResponse<T>> createAPISuccessResponse(T dto, AbstractRestMetaData restMetaData) {
@@ -8,6 +10,16 @@ public abstract class AbstractAPIResponse<T> {
                 "success",
                 RestResponseMessage.USERS_GET_SUCCESS,
                 dto,
+                restMetaData
+        ) {};
+        return ResponseEntity.ok(restResponse);
+    }
+
+    public ResponseEntity<AbstractRestResponse<Collection<T>>> createAPISuccessResponseCollection(Collection<T> dtoCollection, AbstractRestMetaData restMetaData) {
+        AbstractRestResponse<Collection<T>> restResponse = new AbstractRestResponse<>(
+                "success",
+                RestResponseMessage.USERS_GET_SUCCESS,
+                dtoCollection,
                 restMetaData
         ) {};
         return ResponseEntity.ok(restResponse);
