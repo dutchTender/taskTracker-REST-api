fbc80abd14a956f036505deee9758fa258fcb4e4
li zhang <lzhang421@gmail.com>
2025-02-23 22:46:43 -0500

    updated task api response messages.

--- app/src/test/java/com/zhang/app/spring/controller/web/IntegrationTestSuite.java
+++ app/src/test/java/com/zhang/app/spring/controller/web/IntegrationTestSuite.java
@@ -1,5 +1,4 @@
 package com.zhang.app.spring.controller.web;
-
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
--- common/src/main/java/com/zhang/common/base/rest/RestResponseMessage.java
+++ common/src/main/java/com/zhang/common/base/rest/RestResponseMessage.java
@@ -8,9 +8,9 @@ public class RestResponseMessage{
     public static final String USERS_GET_SUCCESS = "Users retrieved successfully"; // is constant because it's used for the controller mapping
 
 
-    public static final String TASK_CREATE_SUCCESS = "New User created successfully";
-    public static final String TASK_UPDATE_SUCCESS = "User update successfully";
-    public static final String TASK_DELETE_SUCCESS = "User deleted successfully";
-    public static final String TASK_GET_SUCCESS = "User retrieved successfully";
-    public static final String TASKS_GET_SUCCESS = "Users retrieved successfully"; // is constant because it's used for the controller mapping
+    public static final String TASK_CREATE_SUCCESS = "New Task created successfully";
+    public static final String TASK_UPDATE_SUCCESS = "Task update successfully";
+    public static final String TASK_DELETE_SUCCESS = "Task deleted successfully";
+    public static final String TASK_GET_SUCCESS = "Task retrieved successfully";
+    public static final String TASKS_GET_SUCCESS = "Tasks retrieved successfully"; // is constant because it's used for the controller mapping
 }
