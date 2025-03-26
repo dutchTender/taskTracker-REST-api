809497a51e47e212d55e6673953e08891cde9aa5
li zhang <lzhang421@gmail.com>
2025-03-26 13:23:27 -0400

    fiexed issue with package scan. also added e2e test.

---
+++ app/src/test/java/com/zhang/app/spring/controller/web/e2eTaskCreateTest.java
@@ -0,0 +1,12 @@
+package com.zhang.app.spring.controller.web;
+
+
+import com.zhang.app.spring.UmContextConfig;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration( classes = {UmContextConfig.class} )
+public class e2eTaskCreateTest {
+}
